f16ce7f68f2811120771ff3510497029efdac055
Amin Fallahi <amin.fallahi@gmail.com>
2019-11-23 19:11:07 -0500

    directory.h

--- filesys/directory.h
+++ filesys/directory.h
@@ -21,7 +21,7 @@
 
 #define FileNameMaxLen 		9	// for simplicity, we assume 
 					// file names are <= 9 characters long
-
+#define NumDirEntries       64
 // The following class defines a "directory entry", representing a file
 // in the directory.  Each entry gives the name of the file, and where
 // the file's header is to be found on disk.
@@ -36,6 +36,7 @@ class DirectoryEntry {
 					//   FileHeader for this file 
     char name[FileNameMaxLen + 1];	// Text name for file, with +1 for 
 					// the trailing '\0'
+    bool isDir;
 };
 
 // The following class defines a UNIX-like "directory".  Each entry in
@@ -61,7 +62,7 @@ class Directory {
     int Find(char *name);		// Find the sector number of the 
 					// FileHeader for file: "name"
 
-    bool Add(char *name, int newSector);  // Add a file name into the directory
+    bool Add(char *name, int newSector, int directory);  // Add a file name into the directory
 
     bool Remove(char *name);		// Remove a file from the directory
 
@@ -70,6 +71,12 @@ class Directory {
     void Print();			// Verbose print of the contents
 					//  of the directory -- all the file
 					//  names and their contents.
+    
+	bool IsDirectory(char *name);
+	void RecursiveList();
+
+	DirectoryEntry *getTable(){return table;}
+	int getTableSize(){return tableSize;}
 
   private:
     int tableSize;			// Number of directory entries
